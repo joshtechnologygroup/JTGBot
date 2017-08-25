1822f8388c2ea861eb6cb01ac8579e5fe47d764e
Rajat Bhatnagar <rajat.bhatnagar@joshtechnologygroup.com>
2017-08-26 00:22:10 +0530

    Add base structure

--- bot/views.py
+++ bot/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+import json
+import apiai
 
 # Create your views here.
 from rest_framework.response import Response
@@ -8,5 +9,20 @@ from rest_framework.views import APIView
 class BotApi(APIView):
     authentication_classes = []
 
-    def get(self, *args, **kwargs):
-        return Response({})
+    def post(self, *args, **kwargs):
+        CLIENT_ACCESS_TOKEN = 'ac3f28e051014b94b5666b89482f3d5c'
+        ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
+        data = self.request.data
+        request = ai.text_request()
+        request.lang = 'en'
+        request.session_id = 'ada6c87f-3048-43e1-8b9d-86dbe6abb90f'
+        request.query = data.get('query', '')
+        response = request.getresponse()
+        response = json.loads(response.read())
+        print response, type(response)
+        if not response['result']['actionIncomplete']:
+            # Intent_name = response['result']['metadata']['intentName']
+            # data = process[Intent_name](a['result'])
+            # Response(data)
+            pass
+        return Response(response['result']['fulfillment']['speech'])
