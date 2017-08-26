6e84104acbf66bb15b1f5dfe905d0ebb515401f0
Rajat Bhatnagar <rajat.bhatnagar@joshtechnologygroup.com>
2017-08-26 17:12:14 +0530

    Handled invalid input

--- bot/views.py
+++ bot/views.py
@@ -12,28 +12,30 @@ class BotApi(APIView):
     def post(self, *args, **kwargs):
         data = self.request.data
         api_response = bot_utils.call_bot_api(data.get('query'), data['email'])
-        if not api_response['result']['actionIncomplete']:
-            intent_name = api_response['result']['metadata']['intentName']
-            if intent_name == 'Contact_Request_Get_By_Name':
-                response = bot_utils.get_contact_info_by_name(api_response['result']['parameters']['JTG_Employee'])
-            elif intent_name == 'Vacation_Query_Available':
-                parameters = api_response['result']['parameters']
-                response = bot_utils.get_official_leaves(
-                    parameters['Vacation_Type_Available'],
-                    parameters['date-period'],
-                )
-            elif intent_name == 'Vacation_Query_Remaining':
-                parameters = api_response['result']['parameters']
-                identity = parameters['JTG_Employee'] or data['email']
-                vaction_type = parameters['Vacation_Type_Remaining']
-                response = bot_utils.get_remaining_leaves_for_user(identity, vaction_type)
-            elif intent_name == 'Vacation_Query_Team_Status':
-                parameters = api_response['result']['parameters']
-                response = bot_utils.get_team_status(parameters['JTG_Team'], parameters['date'])
+        response = 'Time to learn something new'
+        if api_response['status']['code'] == 200:
+            if not api_response['result']['actionIncomplete']:
+                intent_name = api_response['result']['metadata']['intentName']
+                if intent_name == 'Contact_Request_Get_By_Name':
+                    response = bot_utils.get_contact_info_by_name(api_response['result']['parameters']['JTG_Employee'])
+                elif intent_name == 'Vacation_Query_Available':
+                    parameters = api_response['result']['parameters']
+                    response = bot_utils.get_official_leaves(
+                        parameters['Vacation_Type_Available'],
+                        parameters['date-period'],
+                    )
+                elif intent_name == 'Vacation_Query_Remaining':
+                    parameters = api_response['result']['parameters']
+                    identity = parameters['JTG_Employee'] or data['email']
+                    vaction_type = parameters['Vacation_Type_Remaining']
+                    response = bot_utils.get_remaining_leaves_for_user(identity, vaction_type)
+                elif intent_name == 'Vacation_Query_Team_Status':
+                    parameters = api_response['result']['parameters']
+                    response = bot_utils.get_team_status(parameters['JTG_Team'], parameters['date'])
+                else:
+                    response = {}
             else:
-                response = {}
-        else:
-            response = api_response['result']['fulfillment']['speech']
+                response = api_response['result']['fulfillment']['speech']
         return Response(response)
 
 
